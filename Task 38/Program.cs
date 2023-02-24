56e49f27b5f2b1d80f30a3f2ec5f0401adb3f505
Nik <ice_fire50@mail.ru>
2023-02-24 16:30:39 +0300

    task end

---
+++ Task 38/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+int[] arry(int size){
+    int[] arr = new int[size];
+    for(int i=0; i<size; i++) arr[i]=new Random().Next(0, 100);
+    return arr;
+}
+int res(int [] arr){ int Max =arr[1]; int Min = arr[1];
+    for(int i=1; i<arr.Length; i++){
+        if (arr[i]>Max) Max=arr[i];
+        if (arr[i]<Min) Min=arr[i];
+    } 
+return (Max-Min); 
+}
+
+Console.Clear();
+Console.WriteLine("Введите разрядность массива");
+int size = int.Parse(Console.ReadLine());
+int [] mass=arry(size);
+Console.WriteLine($"[ {string.Join(" ", mass)} ]");
+Console.WriteLine("разницу между максимальным и минимальным элементов массива: " + res(mass));
---
+++ task 36/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int[] arry(int size){
+    int[] arr = new int[size];
+    for(int i=0; i<size; i++) arr[i]=new Random().Next(100, 1000);
+    return arr;
+}
+int sum(int [] arr){ int S=0;
+    for(int i=1; i<arr.Length; i=i+2) S=S+arr[i];
+    return S; 
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите разрядность массива");
+int size = int.Parse(Console.ReadLine());
+int [] mass=arry(size);
+Console.WriteLine($"[ {string.Join(" ", mass)} ]");
+Console.WriteLine("сумма элементов на нечётных позициях: " + sum(mass));
