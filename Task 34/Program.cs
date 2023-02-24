fd3e75352f2447684b84450efda33d3e84f0ce28
Nik <ice_fire50@mail.ru>
2023-02-24 16:04:05 +0300

    task 34

---
+++ Task 34/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arry(int size){
+    int[] arr = new int[size];
+    for(int i=0; i<size; i++) arr[i]=new Random().Next(100, 1000);
+    return arr;
+}
+
+int namber(int [] mass){
+int N=0;
+    for (int i=0; i<mass.Length;i++) if (mass[i]%2==0) N++;
+return N;
+}
+
+Console.Clear();
+Console.WriteLine("Введите разрядность массива");
+int size = int.Parse(Console.ReadLine());
+int [] mass=arry(size);
+Console.WriteLine($"[ {string.Join(" ", mass)} ]");
+Console.WriteLine(namber(mass));
